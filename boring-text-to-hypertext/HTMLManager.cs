fc555e0e48454a3c49504cd5706d461991e3c00e
Samuel Robinson <pinkstarsr11@gmail.com>
2018-07-21 02:58:26 -0700

    Added files

---
+++ boring-text-to-hypertext/HTMLManager.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.IO;
+using System.Reflection;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BoringTextToHypertext {
+    internal static class HTMLManager {
+
+		private const string SchemaFile = "Schema.html";
+		private const string SchemaTitle = "{TITLE}";
+		private const string SchemaBody = "{BODY}";
+		private const string HTMLFolder = "HTML";
+
+		private static string schema = string.Empty;
+
+		internal static bool Initialize() {
+			if(File.Exists(SchemaFile)) {
+				try {
+
+					schema = File.ReadAllText(SchemaFile);
+
+					return true;
+
+				} catch(IOException exception) {
+
+					Console.WriteLine(exception.Message);
+
+					return false;
+				}
+			} else {
+				Console.WriteLine($"Could not find a schema file in executing directory ({SchemaFile})");
+				return false;
+			}
+		}
+
+		internal static void GetHTMLForLine(string line,ref StringBuilder builder) {
+
+			line.Trim();
+
+			line = line.Replace("<","&lt;");
+
+			line = line.Replace(">","&gt;");
+			
+			builder.AppendLine($"		<p>{line}<p>");
+
+		}
+
+		internal static string GetFilePath(string originalPath) {
+			return Path.Combine(
+				HTMLFolder,
+				Path.ChangeExtension(originalPath,".html")
+			);
+		}
+
+		internal static void WriteHTMLFile(string path,string HTMLBody) {
+
+			string file = schema;
+			string HTMLTitle = Path.GetFileNameWithoutExtension(path);
+
+			file = file.Replace(SchemaTitle,HTMLTitle);
+			file = file.Replace(SchemaBody,HTMLBody);
+
+			Directory.CreateDirectory(HTMLFolder);
+
+			File.WriteAllText(path,file);
+
+		}
+	}
+}
--- boring-text-to-hypertext/Program.cs
+++ boring-text-to-hypertext/Program.cs
@@ -1,12 +1,131 @@
 ﻿using System;
+using System.Text;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
 
-namespace boring_text_to_hypertext
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
-        }
-    }
+namespace BoringTextToHypertext {
+	internal sealed class Program {
+
+		private static void Main(string[] args) {
+
+			if(!HTMLManager.Initialize()) {
+
+				Console.WriteLine("There was a failure initializing HTMLManager");
+				Console.ReadKey(true);
+
+				return;
+			}
+
+			if(args != null && args.Length > 0) {
+
+				foreach(string arg in args) {
+
+					ProcessFile(arg);
+
+				}
+
+				Console.WriteLine("Batch processing complete");
+
+				Program.Main(null);
+
+			} else {
+
+				HTMLManager.Initialize();
+
+				while(true) {
+
+					Console.Write("File path: ");
+					ProcessFile(Console.ReadLine().Trim());
+
+				}
+			}
+		}
+
+		private static void ProcessFile(string path) {
+
+
+			if(File.Exists(path)) {
+
+				string HTMLBody = null;
+
+				try {
+
+					using(StreamReader reader = File.OpenText(path)) {
+						HTMLBody = GenerateHTMLBody(reader);
+					}
+
+				} catch (IOException exception) {
+
+
+					Console.WriteLine($"Failure opening \"{path}\"");
+
+					Console.WriteLine(exception.Message);
+
+					return;
+				}
+
+
+				var exportPath = HTMLManager.GetFilePath(path);
+
+				if(File.Exists(exportPath)) {
+
+					bool @continue = false;
+					Console.WriteLine("Do you want to overwrite the already existing file? (Y/N)");
+
+					while(!@continue) {
+						switch(Console.ReadKey(true).Key) {
+							case ConsoleKey.Y:
+								@continue = true;
+								break;
+							case ConsoleKey.N:
+								Console.WriteLine($"Stopped {path} conversion");
+								return;
+						}
+					}
+				}
+
+				try {
+
+					HTMLManager.WriteHTMLFile(exportPath,HTMLBody);
+
+				} catch(IOException exception) {
+
+					Console.WriteLine($"Failure writing {path} to {exportPath}");
+
+					Console.WriteLine(exception.Message);
+
+					return;
+
+				}
+
+				Console.WriteLine($"Saved {path} to {exportPath}!");
+
+			} else {
+
+				Console.WriteLine($"{path} (as a file) does not exist");
+
+			}
+		}
+
+
+		private static string GenerateHTMLBody(StreamReader reader) {
+			var stringBuilder = new StringBuilder();
+
+			while(!reader.EndOfStream) {
+
+				string line = reader.ReadLine();
+
+				HTMLManager.GetHTMLForLine(line,ref stringBuilder);
+
+
+			}
+
+			return stringBuilder.ToString();
+
+
+		}
+
+
+	}
 }
